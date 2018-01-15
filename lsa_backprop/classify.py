1c277c66490213467f352be8f5ffe5bc6e184e9d
vlejd <vladimir.v.macko@gmail.com>
2018-01-15 22:33:12 +0100

    Derivation acording to term weight.

--- lsa_backprop/classify.py
+++ lsa_backprop/classify.py
@@ -1,3 +1,4 @@
+from sklearn.linear_model import LogisticRegression
 from scipy.stats import logistic
 import numpy as np
 
@@ -21,10 +22,39 @@ class Classifier(object):
         self.b -= db
         self.w -= dw
         
-    def gradient(X, Y):
-        pass
+    def dX(self, X, Y):
+        h = (X*self.w).sum(axis=1) + self.b
+        Yhat = logistic.cdf(h)
+        dif = Yhat-Y
+        sigma = dif * Yhat * (1-Yhat)
+        dx = (sigma.reshape(-1,1)* self.w)
     
     def predict(self, X):
         h = (X*self.w).sum(axis=1) + self.b
         Yhat = logistic.cdf(h)
         return (Yhat>0.5)+0
+
+
+class SkClassifier(object):
+    def __init__(self, params=None):
+        if params is None:
+            params = {}
+        self.raw_cls = LogisticRegression(**params)
+
+    def fit(self, X, Y, epoch=1000):
+        self.raw_cls.fit(X, Y)
+            
+    def dx(self, X, Y):
+        b = self.raw_cls.intercept_[0]
+        theta = self.raw_cls.coef_[0]
+
+        h = (X*theta).sum(axis=1) + b
+        Yhat = logistic.cdf(h)
+        dif = Yhat-Y
+        sigma = dif * Yhat * (1-Yhat)
+        dx = (sigma.reshape(-1,1)* theta)
+        return dx
+
+    
+    def predict(self, X):
+        return self.raw_cls.predict(X)
