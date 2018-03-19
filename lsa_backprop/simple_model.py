0f063da56285a024a38acb2c97dfa1d3c1c72b6b
vlejd <vladimir.v.macko@gmail.com>
2018-03-19 10:55:04 +0100

    Propper dumping for experiments

--- lsa_backprop/simple_model.py
+++ lsa_backprop/simple_model.py
@@ -8,26 +8,28 @@ class SimpleModel(object):
     SCHEMES = ('None', 'tfidf', 'tfchi2', 'tfig', 'tfgr', 'tfor', 'tfrf')
     BASIC_SCHEMES = ('None', 'tfidf', 'tfchi2')
 
-    TRANSFORMERS = { 
-        'tfidf': UnsupervisedTfidfTransformer(norm=None),
-        'tfchi2': SupervisedTermWeightingWTransformer(scheme='tfchi2'),
-        'tfig': SupervisedTermWeightingWTransformer(scheme='tfig'),
-        'tfgr': SupervisedTermWeightingWTransformer(scheme='tfgr'),
-        'tfor': SupervisedTermWeightingWTransformer(scheme='tfor'),
-        'tfrf': SupervisedTermWeightingWTransformer(scheme='tfrf'),
-        'None': None,
-        None: None
-    }
-
-    def __init__(self, cls=None, weights=None, w=None, use_svd=True):
+    def __init__(self, cls=None, weights='None', w=None, use_svd=True, svd_dim=None):
         self.cls = cls
         if self.cls is None:
-            self.cls = LogisticRegression()
-        self.weight_model = self.TRANSFORMERS[weights] 
-        self.w = w
-        self.use_svd = use_svd
-        
+            raise("No classifier")
 
+        TRANSFORMERS = { 
+            'tfidf': UnsupervisedTfidfTransformer(norm=None),
+            'tfchi2': SupervisedTermWeightingWTransformer(scheme='tfchi2'),
+            'tfig': SupervisedTermWeightingWTransformer(scheme='tfig'),
+            'tfgr': SupervisedTermWeightingWTransformer(scheme='tfgr'),
+            'tfor': SupervisedTermWeightingWTransformer(scheme='tfor'),
+            'tfrf': SupervisedTermWeightingWTransformer(scheme='tfrf'),
+            'None': None,
+            None: None
+        }
+        self.weight_model = TRANSFORMERS[weights] 
+        if w is None:
+            self.w = None
+        else:
+            self.w = np.copy(w)
+        self.use_svd = use_svd
+        self.svd_dim = svd_dim if svd_dim is not None else 200
 
     def fit(self, X, Y):
         self.dictionary = gensim.corpora.Dictionary(X)
@@ -38,13 +40,12 @@ class SimpleModel(object):
             self.weight_model.fit(bow, Y)
             bow = self.weight_model.transform(bow)
 
-        self.bow = bow
         if self.w is None:
             self.w = np.ones(self.num_terms)
         bow = bow.multiply(self.w)
         if self.use_svd:
             bow = gensim.matutils.Sparse2Corpus(bow.T)
-            self.lsi = gensim.models.LsiModel(bow, id2word=self.dictionary)
+            self.lsi = gensim.models.LsiModel(bow, num_topics=self.svd_dim, id2word=self.dictionary, extra_samples=200, power_iters=3)
             self.corpus = gensim.matutils.corpus2dense(self.lsi[bow], self.lsi.num_topics).T
         else:
             self.corpus = bow.multiply(self.w)
@@ -52,6 +53,8 @@ class SimpleModel(object):
         self.cls.fit(self.corpus, Y)
     
     def dw(self, X, Y):
+        if not self.use_svd:
+            raise('Derivation can be used only wth svd') 
         _ = self.predict(X)
         self.d_embedding = self.cls.dx(self.embedding, Y)
         u = self.lsi.projection.u
@@ -66,7 +69,7 @@ class SimpleModel(object):
 
         if self.weight_model is not None:
             bow = self.weight_model.transform(bow)
-
+        self.bow = bow
         if self.use_svd:
             bow = bow.multiply(self.w)
             bow = gensim.matutils.Sparse2Corpus(bow.T)
