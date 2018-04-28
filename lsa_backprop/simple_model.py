068f44d4b7e8df4bd430bf3a38a63b4b21f58dc0
vlejd <vladimir.v.macko@gmail.com>
2018-04-28 11:36:29 +0200

    internal w fix

--- lsa_backprop/simple_model.py
+++ lsa_backprop/simple_model.py
@@ -53,7 +53,6 @@ class SimpleModel(object):
 
 
     def fit(self, X, Y):
-        self.internal_w = None
         self.dictionary = gensim.corpora.Dictionary(X)
         self.num_terms = len(self.dictionary.dfs)
         bow = list(map(self.dictionary.doc2bow, X))
