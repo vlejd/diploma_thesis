278bdecd9a9c4ce98aafc297402ec1c8e900d8b9
vlejd <vladimir.v.macko@gmail.com>
2018-05-01 10:51:55 +0200

    All results

--- lsa_backprop/utils.py
+++ lsa_backprop/utils.py
@@ -1,4 +1,6 @@
 import datasets
+import seaborn as sns
+sns.set()
 
 def subtract_baseline(df):
     for d in datasets.ALL_DATASETS + datasets.TREC_DATASETS:
@@ -25,3 +27,13 @@ def multireplace(txt, lis):
     for q,w in lis:
         txt = txt.replace(q,w)
     return txt
+
+cm = sns.light_palette("green", as_cmap=True)
+def color_positives(val):
+    color = 'red' if val > 0 else 'black'
+    return 'color: %s' % color
+
+
+TREC = ['TRECDataset-ABBR', 'TRECDataset-DESC', 'TRECDataset-ENTY', 'TRECDataset-HUM', 'TRECDataset-LOC', 'TRECDataset-NUM']
+NOTREC = ['CRDataset', 'MPQADataset', 'MRDataset', 'SUBJDataset']
+
